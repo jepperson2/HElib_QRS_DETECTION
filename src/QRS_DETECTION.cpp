4648e3aa3bb4bcc175d83975bc5750e9755796b1
Jesse Epperson <Epperson.JesseE@gmail.com>
2017-09-26 15:33:37 +0800

    added infrastructure to run algorithm and test more smoothly; changed classname

--- src/QRS_DETECTION.cpp
+++ src/QRS_DETECTION.cpp
@@ -1,24 +1,42 @@
-#include "QRS_DETECTION.h"
+#include "qrs_detection.h"
 
-QRS_DETECTION::QRS_DETECTION(vector<double> ecgs, int sampling_frequency, bool dbg){
+/*TODO: 
+    move stuff in initialize that prepares/encrypts data
+    create methods to match .h file
+    finish making methods in .h file (calls to actual algorithms)
+    write calls to algorithsm to call functions defined in h files (constructor() -> loop through specific (int iterations = samples.size/(nslots-n_considered)
+    rethink through parameters/outputs in .h file new methods
+    implement baby example in plain
+    think through security of methods in .h -> reveal info by return values? can hide. 
+    implement min_max based on that one paper's method 3
+    change parameters (params.L calculation) based on expected levels needed for subtration/comparison. 
+    double check that comparison works on neg numbers 
+    if not, scale everything...? need to make sure that works... 
+    
+
+  */
+QRS_Detection::QRS_Detection(vector<long> ecgs, int sampling_frequency, bool dbg){
 	samples = ecgs;
     fs = sampling_frequency;
     debug = dbg;
 
-    bits = 16;
+    bits = 64;
 
 	set_params();
 	initialize();
 }
-const string QRS_DETECTION::className(){
-	return "QRS_DETECTION";
+
+const string QRS_Detection::className(){
+	return "QRS_Detection";
 }
-void QRS_DETECTION::t_start(){
+
+void QRS_Detection::t_start(){
 	if (debug){
 		t.start();
 	}
 }
-void QRS_DETECTION::t_end(string name){
+
+void QRS_Detection::t_end(string name){
 	if (debug){
 		double duration = t.end("silent");
 		cout << className() << ": " << name << " - " << nslots << " operations done in " << duration << "s" << endl;
@@ -26,20 +44,18 @@ void QRS_DETECTION::t_end(string name){
 	}
 }
 
-Errors QRS_DETECTION::test(){
-	Errors e("QRS_DETECTION");
-//	e.add("RIPPLE CARRY ADDER sequential circuit", test_RCADDER());
-//	e.add("RIPPLE BORROW SUBTRACTOR sequential circuit", test_RBSUBER());
-//	e.add("RIPPLE COMPARATOR sequential circuit", test_RCMP());
-	e.add("DUAL SLOPE algorithm test", test_DUALSLOPE());
-//	e.add("RIPPLE EQUALITY COMPARATOR sequential circuit", test_REQ());
-//	e.add("SHIFT RIGHT sequential circuit", test_SHIFTR());
-//	e.add("SHIFT LEFT sequential circuit", test_SHIFTL());
-//	e.add("MUX sequential circuit", test_NMUX());
+Errors QRS_Detection::test_all(){
+	Errors e("QRS_Detection");
+	e.add("ds_fhe()", test_ds_fhe());
+	e.add("ds_fhe(5)", test_ds_fhe(5));
+	e.add("ds_fhe_unpacked()", test_ds_unpacked_fhe());
+	e.add("ds_fhe_unpacked(5)", test_ds_unpacked_fhe(5));
+	e.add("ds_plain()", test_ds_plain());
+	e.add("ds_plain(5)", test_ds_plain(5));
 	return e;
 }
 
-void QRS_DETECTION::set_params(){
+void QRS_Detection::set_params(){
 	params.p = 2;
 	params.r = 1;
 	params.d = 0; // field p^d
@@ -104,8 +120,6 @@ void QRS_DETECTION::set_params(){
 		}
 	}
 	
-	
-	
 	if ((params.L > 42)&&(params.slb > 600)){
 		params.L *= 1.2;
 	}
@@ -114,7 +128,7 @@ void QRS_DETECTION::set_params(){
 	
 	params.m = FindM(params.k,params.L,params.c,params.p,params.d,params.slb,0);
 }
-void QRS_DETECTION::initialize(){
+void QRS_Detection::initialize(){
     if (debug){
         cout << className() << ": Initializing..." << endl;
     }   
@@ -124,7 +138,7 @@ void QRS_DETECTION::initialize(){
     double double_b = 0.063 * fs;   // set as in Wang's paper, an estimate of the max width of QRS complex
     b = round(double_b);
     
-    n_samples = (2 * b) + 1;        // number of samples considered in one iteration (goes from 0 to -2b)
+    n_considered = (2 * b) + 1;     // number of samples considered in one iteration (goes from 0 to -2b)
     lr_size = (b - a) + 1;          // convenience for looping through each side 
 
     diff_threshold = 3840 / fs;     // initial diff_threshold value. to be updated later based on S_ave
@@ -144,38 +158,128 @@ void QRS_DETECTION::initialize(){
 
     if (debug){
         cout << "a,b = " << a << "," << b << endl;
-        cout << "n_samples = " << n_samples << endl;
+        cout << "n_considered = " << n_considered << endl;
         cout << "lr_size = " << lr_size << endl;
 
         for (int i = 0; i < lr_size; i++){
             cout << "sample_difference_widths[" << i << "]: " << sample_difference_widths[i] << endl;
         }
     }
-	N_numbers = n_samples;
+	n_samples = samples.size(); //Total number of samples given to process 
 	he.debug_on(debug);
 	cout << className() << ": Number of bits n was set to " << bits << endl; 
 	nslots = he.keyGen(params);
 	mkt k_ones = he.setOnes(nslots);
 	he.set01(k_ones);
-	inputs.resize(N_numbers, vector < long > (nslots,0));
-	v_in.resize(N_numbers,vector< vector<long> >(bits,vector<long>(nslots,0)));
-    cout << "v_in size: " << v_in.size() << endl;
-    cout << "v_in[0] size: " << v_in[0].size() << endl;
-    cout << "v_in[0][0] size: " << v_in[0][0].size() << endl;
-	k_constant.resize(N_numbers, vector < mkt>(bits));
+
+	// Compute scaling_factors based on a and b values. 
+    long x = 5354228880;
+    // x = 5,354,228,880 = 2^4*3^2*5*7*11*13*17*19*23 = Minimum number divisible by 10, 11, ... , 23
+    // Note: If fs != 360, then likely a != 10 and b != 23, which means x should be recomputed to be the minimum value such that x / a, x / (a+1), ... , x / b  all yield whole numbers 
+
+    scaling_factors.push_back(x);
+    for (int i = a; i <= b; i++){
+        scaling_factors.push_back(x/i);
+    }
+
+    // Compute samples*x, samples*x/a, ... , samples*x/b
+    for (int i = 0; i < nslots; i++){
+        samples_x.push_back(samples[i]*scaling_factors[0]);
+        samples_x_10.push_back(samples[i]*scaling_factors[1]);
+        samples_x_11.push_back(samples[i]*scaling_factors[2]);
+        samples_x_12.push_back(samples[i]*scaling_factors[3]);
+        samples_x_13.push_back(samples[i]*scaling_factors[4]);
+        samples_x_14.push_back(samples[i]*scaling_factors[5]);
+        samples_x_15.push_back(samples[i]*scaling_factors[6]);
+        samples_x_16.push_back(samples[i]*scaling_factors[7]);
+        samples_x_17.push_back(samples[i]*scaling_factors[8]);
+        samples_x_18.push_back(samples[i]*scaling_factors[9]);
+        samples_x_19.push_back(samples[i]*scaling_factors[10]);
+        samples_x_20.push_back(samples[i]*scaling_factors[11]);
+        samples_x_21.push_back(samples[i]*scaling_factors[12]);
+        samples_x_22.push_back(samples[i]*scaling_factors[13]);
+        samples_x_23.push_back(samples[i]*scaling_factors[14]);
+    }
+    
+
+    samples_bits_x.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_10.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_11.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_12.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_13.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_14.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_15.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_16.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_17.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_18.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_19.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_20.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_21.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_22.resize(bits, vector<long> (nslots,0));
+    samples_bits_x_23.resize(bits, vector<long> (nslots,0));
+
+    for (int i = 0; i < nslots; i++){
+        bitset<64> b_x(samples_x[i]);
+        bitset<64> b_x_10(samples_x_10[i]);
+        bitset<64> b_x_11(samples_x_11[i]);
+        bitset<64> b_x_12(samples_x_12[i]);
+        bitset<64> b_x_13(samples_x_13[i]);
+        bitset<64> b_x_14(samples_x_14[i]);
+        bitset<64> b_x_15(samples_x_15[i]);
+        bitset<64> b_x_16(samples_x_16[i]);
+        bitset<64> b_x_17(samples_x_17[i]);
+        bitset<64> b_x_18(samples_x_18[i]);
+        bitset<64> b_x_19(samples_x_19[i]);
+        bitset<64> b_x_20(samples_x_20[i]);
+        bitset<64> b_x_21(samples_x_21[i]);
+        bitset<64> b_x_22(samples_x_22[i]);
+        bitset<64> b_x_23(samples_x_23[i]);
+        for (int b = 0; b < bits; b++){
+            samples_bits_x[b][i] = b_x[b];
+            samples_bits_x_10[b][i] = b_x_10[b];
+            samples_bits_x_11[b][i] = b_x_11[b];
+            samples_bits_x_12[b][i] = b_x_12[b];
+            samples_bits_x_13[b][i] = b_x_13[b];
+            samples_bits_x_14[b][i] = b_x_14[b];
+            samples_bits_x_15[b][i] = b_x_15[b];
+            samples_bits_x_16[b][i] = b_x_16[b];
+            samples_bits_x_17[b][i] = b_x_17[b];
+            samples_bits_x_18[b][i] = b_x_18[b];
+            samples_bits_x_19[b][i] = b_x_19[b];
+            samples_bits_x_20[b][i] = b_x_20[b];
+            samples_bits_x_21[b][i] = b_x_21[b];
+            samples_bits_x_22[b][i] = b_x_22[b];
+            samples_bits_x_23[b][i] = b_x_23[b];
+        }
+    }
+
+	k_constant_x.resize(bits);
+/*
+    for (int i = 0; i < bits; i++){
+        k_constant_x[i] = he.encrypt(samples_bits_x[i]);
+        cout << "encrypted as: " << k_constant_x[i] << endl;
+    }
+*/ 
+
+    inputs.resize(n_considered, vector < long > (nslots,0));
+	v_in.resize(n_considered,vector< vector<long> >(bits,vector<long>(nslots,0)));
+	k_constant.resize(n_considered, vector < mkt>(bits));
 
 	//inputs to N bit circuits
-	for(unsigned i = 0; i < nslots; i++){
-		inputs[0][i] = rand() % (unsigned)pow(2,bits);
-		inputs[1][i] = rand() % (unsigned)pow(2,bits);
-
-		if(debug){
-			cout << "inputs[0][" << i << "], inputs[1][" << i << "]: " << inputs[0][i] << ", " << inputs[1][i] << endl;
-		}
+	for(unsigned j = 0; j < nslots; j++){  
+		    inputs[0][j] = samples_x[j];
+            inputs[1][j] = samples_x_10[j];
 	}
 	
+	if(debug){
+        for(unsigned j = 0; j < nslots; j++){ 
+			    cout << "inputs[0][" << j << "]: " << inputs[0][j] << endl;
+                cout << "inputs[1][" << j << "]: " << inputs[1][j] << endl;
+        }
+    }
+/*
 	//Converts inputs to bits into v_in for parallel ciphertexts
-	for(unsigned n = 0; n < N_numbers; n++){
+	for(unsigned n = 0; n < n_considered; n++){
 		for(unsigned j = 0; j < nslots; j++){
 			bitset<64> bin(inputs[n][j]); //max is 2^64 so max nbits = 64
 			for(unsigned b = 0; b < bits; b++){
@@ -186,200 +290,173 @@ void QRS_DETECTION::initialize(){
 	
 	//Encrypts all the vectors into ciphertexts
 	if(debug){
-		cout << className() << ": Encrypting input vectors (" << N_numbers * bits << " vectors)" << endl;
+		cout << className() << ": Encrypting input vectors (" << n_considered * bits << " vectors)" << endl;
 	}
-	for(unsigned n = 0; n < N_numbers; n++){
+	for(unsigned n = 0; n < n_considered; n++){
 		for (unsigned b = 0; b < bits; b++){
 			k_constant[n][b] = he.encrypt(v_in[n][b]);
 			if(debug){
 				cout << "k_constant[" << n << "][" << b << "]: " << k_constant[n][b] << endl; 
 			}
 		}
-	}
-}
-void QRS_DETECTION::make_copies(){
-	for(unsigned n = 0; n < k.size(); n++){
-		for (unsigned b = 0; b < k[n].size(); b++){
-			he.erase(k[n][b]);
-		}
-	}
-	k = vector< vector<mkt> >(N_numbers, vector<mkt>(bits));
-	for(unsigned n = 0; n < N_numbers; n++){
-		for (unsigned b = 0; b < bits; b++){
-			k[n][b] = he.copy(k_constant[n][b]);
-		}
-	}
-}
-bool QRS_DETECTION::test_RCADDER(){
-	//NO 2's complement, MSB is used for "overflow" (last carry out)
-	make_copies();
-	t_start();
-	he.RCADDER(k[0],k[1]);
-	t_end(__FUNCTION__);
-	vector<long> results(nslots);
-	results = conv.matrix2LongVec(he.decryptNbits(k[0]));
-	for (unsigned i = 0; i < nslots; i++){ //bit level
-		if (results[i] != (inputs[0][i] + inputs[1][i])){
-			return true;
-		}
-	}
-	return false;
+	}*/
 }
 
-bool QRS_DETECTION::test_RBSUBER(){
-	make_copies();
-	string bin_str;
-	t_start();
-	he.RBSUBER(k[0],k[1]);
-	t_end(__FUNCTION__);
-	vector<long> results(nslots);
-	results = conv.matrix2SignedLongVec(he.decryptNbits(k[0]));
-	for (unsigned i = 0; i < nslots; i++){ //bit level
-		if (results[i] != (inputs[0][i] - inputs[1][i])){
-			return true;
+void QRS_Detection::make_copies(vector< vector<mkt> > input, vector< vector<mkt> > destination){
+	for(unsigned n = 0; n < destination.size(); n++){
+		for (unsigned b = 0; b < destination[n].size(); b++){
+			he.erase(destination[n][b]);
 		}
 	}
-	return false;
-}
-bool QRS_DETECTION::test_RCMP(){
-	make_copies();
-	t_start();
-	he.RCMP(k[0],k[1]);
-	t_end(__FUNCTION__);
-	
-	vector< vector<long> > v_out1, v_out2;
-	v_out1 = he.decryptNbits(k[0]);
-	v_out2 = he.decryptNbits(k[1]);
-
-	if(debug){
-		for(unsigned i = 0; i < nslots; i++){
-			cout << "v_out1[0][" << i << "], v_out2[0][" << i << "]: " << v_out1[0][i] << ", " << v_out2[0][i] <<  endl;  
+	destination = vector< vector<mkt> >(input.size(), vector<mkt>(input[0].size())); // used to be (n_considered, vector<mkt>(bits))
+	for(unsigned n = 0; n < input.size(); n++){
+		for (unsigned b = 0; b < input[n].size(); b++){
+			destination[n][b] = he.copy(input[n][b]);
 		}
 	}
-
-	for (unsigned i = 0; i < nslots; i++){
-		if (v_out1[0][i] != (inputs[0][i] == inputs[1][i])){
-			return true;
-		}
-		if (v_out2[0][i] != (inputs[0][i] > inputs[1][i])){
-			return true;
-		}
-		if ((!v_out1[0][i] && !v_out2[0][i]) != (inputs[0][i] < inputs[1][i])){
-			return true;
-		}
-	}
-	return false;
 }
 
-bool QRS_DETECTION::test_DUALSLOPE(){
-	make_copies();
-	t_start();
-//	he.EFF_RCMP(k[0],k[1]);
-	he.RCMP(k[0],k[1]);
-	t_end(__FUNCTION__);	
+void QRS_Detection::prepare_data(vector<long> sample_subset){
 
-	vector< vector<long> > v_out1, v_out2;
-	v_out1 = he.decryptNbits(k[0]);
-	v_out2 = he.decryptNbits(k[1]);
-
-	if(debug){
-                for(unsigned i = 0; i < nslots; i++){
-                        cout << "v_out1[0][" << i << "], v_out2[0][" << i << "]: " << v_out1[0][i] << ", " << v_out2[0][i] <<  endl;
-                }
-        }
-
-	for (unsigned i = 0; i < nslots; i++){
-                if (v_out1[0][i] != (inputs[0][i] == inputs[1][i])){
-                        return true;
-                }
-                if (v_out2[0][i] != (inputs[0][i] > inputs[1][i])){
-                        return true;
-                }
-                if ((!v_out1[0][i] && !v_out2[0][i]) != (inputs[0][i] < inputs[1][i])){
-                        return true;
-                }
-        }
-
-	return false;
 }
 
-bool QRS_DETECTION::test_REQ(){
-	make_copies();
-	t_start();
-	he.REQ(k[0],k[1]);
-	t_end(__FUNCTION__);
-	vector<long> results(nslots);
-	results = conv.matrix2LongVec(he.decryptNbits(k[0]));
-	for (unsigned i = 0; i < nslots; i++){
-		if (results[i] != (inputs[0][i] == inputs[1][i])){
-			return true;
-		}
-	}
-	return false;
-}
-bool QRS_DETECTION::test_SHIFTR(){
-	if(debug){
-		cout << className() << ": Running " << __FUNCTION__ << "..." << endl;
-	}
-	//complexity negligible
-	make_copies();
-	vector<long> results(nslots);
-	unsigned nbits = k[0].size();
-	for (unsigned shift = 0; shift <= nbits; shift++){
-		make_copies();
-		he.SHIFTR(k[0],shift);
-		results = conv.matrix2LongVec(he.decryptNbits(k[0]));
-		for (unsigned i = 0; i < nslots; i++){
-			if ((inputs[0][i] >> shift) != results[i]){
-				return true;
-			}
-		}
-	}
-	return false;
-}
-bool QRS_DETECTION::test_SHIFTL(){
-	if(debug){
-		cout << className() << ": Running " << __FUNCTION__ << "..." << endl;
-	}
-	//complexity negligible
-	make_copies();
-	vector<long> results(nslots);
-	for (unsigned shift = 0; shift <= bits; shift++){
-		make_copies();
-		he.SHIFTL(k[0], shift);
-		results = conv.matrix2LongVec(he.decryptNbits(k[0]));
-		for (unsigned i = 0; i < nslots; i++){
-			if (results[i] != (inputs[0][i] << shift)){	
-				return true;
-			}
-		}
-	}
-	return false;
-}
-bool QRS_DETECTION::test_NMUX(){
-	vector<long> v_zeros(nslots, 0), v_ones(nslots, 1);
-	mkt k_zeros = he.encrypt(v_zeros);
-	mkt k_ones = he.encrypt(v_ones);
-	vector<long> results(nslots);
-	
-	make_copies();
-	t_start();
-	he.NMUX(k[0], k[1], k_zeros);
-	t_end(__FUNCTION__);
-	results = conv.matrix2LongVec(he.decryptNbits(k[0]));
-	for (unsigned i = 0; i < nslots; i++){
-		if (results[i] != inputs[1][i]){	
-			return true;
-		}
-	}
-	make_copies();
-	he.NMUX(k[0], k[1], k_ones);
-	results = conv.matrix2LongVec(he.decryptNbits(k[0]));
-	for (unsigned i = 0; i < nslots; i++){
-		if (results[i] != inputs[0][i]){	
-			return true;
-		}
-	}
-	return false;
+void QRS_Detection::compute_lr_slopes(vector< vector<mkt> > encrypted_pairs, bool simd){
+
 }
 
+void QRS_Detection::compute_lr_slopes(vector< vector<long> > plain_pairs){
+
+}
+
+vector<mkt> QRS_Detection::compute_min_max(vector<mkt> encrypted_list){
+    vector<mkt> result;
+
+
+    return result;
+}
+
+vector<long> QRS_Detection::compute_min_max(vector<long> plain_list){
+    vector<long> result; 
+
+
+    return result;
+}
+
+vector<mkt> QRS_Detection::compute_diff_max(vector<mkt> encrypted_mins_maxs){
+    vector<mkt> result;
+
+    
+    return result;
+}
+
+vector<long> QRS_Detection::compute_diff_max(vector<long> plain_mins_maxs){
+    vector<long> result;
+
+
+    return result;
+}
+
+vector<mkt> QRS_Detection::compare_to_thresholds(vector<mkt> diff_maxs){
+    vector<mkt> result;
+
+    
+    return result;
+}
+
+vector<bool> QRS_Detection::compare_to_thresholds(vector<long> diff_maxs){
+    vector<bool> result;
+
+
+    return result;
+}
+
+mkt QRS_Detection::check_peak_closeness(vector<mkt> peaks){
+    mkt true_peak;
+
+
+    return true_peak;
+}
+
+long QRS_Detection::check_peak_closeness(vector<long> peaks){
+    long true_peak;
+
+
+    return true_peak;
+}
+
+vector<mkt> QRS_Detection::update_thresholds(vector<mkt> diff_maxs){
+    vector<mkt> results;
+
+
+    return results;
+}
+
+void QRS_Detection::update_thresholds(long diff_max){
+
+}
+
+/**********************DUALSLOPE*************************/
+
+void QRS_Detection::ds_fhe(){
+    int iterations_needed = n_samples / (nslots - n_considered);
+    iterations++; // 
+    if (debug){
+        cout << "iterations_needed = " << iterations_needed << endl;
+    }
+}
+
+void QRS_Detection::ds_fhe(int iterations){
+
+    cout << "in ds_fhe(iterations), iterations = " << iterations << endl;
+}
+
+void QRS_Detection::ds_unpacked_fhe(){
+
+    cout << "in ds_unpacked_fhe()" << endl;
+}
+
+void QRS_Detection::ds_unpacked_fhe(int iterations){
+
+    cout << "in ds_unpacked_fhe(iterations), iterations = " << iterations << endl;
+}
+
+void QRS_Detection::ds_plain(){
+
+    cout << "in ds_plain()" << endl;
+}
+
+void QRS_Detection::ds_plain(int iterations){
+    cout << "in ds_plain(iterations), iterations = " << iterations << endl;
+}
+
+/***********************TESTING**************************/
+
+bool QRS_Detection::test_ds_fhe(){
+    ds_fhe();
+    return false;
+}
+
+bool QRS_Detection::test_ds_fhe(int iterations){
+    ds_fhe(iterations);
+    return false;
+}
+
+bool QRS_Detection::test_ds_unpacked_fhe(){
+    ds_unpacked_fhe();
+    return false;
+}
+
+bool QRS_Detection::test_ds_unpacked_fhe(int iterations){
+    ds_unpacked_fhe(iterations);
+    return false;
+}
+
+bool QRS_Detection::test_ds_plain(){
+    ds_plain();
+    return false;
+}
+
+bool QRS_Detection::test_ds_plain(int iterations){
+    ds_plain(iterations);
+    return false;
+}
--- src/QRS_DETECTION.h
+++ src/QRS_DETECTION.h
@@ -5,51 +5,128 @@
 #include <bitset>
 #include <stdlib.h> //rand
 
-class QRS_DETECTION {
+class QRS_Detection {
 	public:
-		QRS_DETECTION(vector<double> ecgs, int sampling_frequency, bool dbg);
-		Errors test();
+		QRS_Detection(vector<long> ecgs, int sampling_frequency, bool dbg);
+		Errors test_all();
 
 	private:
 		const string className();
 		bool debug;
-		HE he;
+		HE he;                  // instance of HE, our API for homomorphic binary circuits 
 		Timing t;
 		Conversion conv;
 
         int fs;                 // Sampling Frequency
-        vector<double> samples;
+        vector<long> samples;
         vector<double> sample_difference_widths;
 
         int a;                  // minimum distance away from considered sample = 0.027*fs
         int b;                  // maximum distance away from considered sample = 0.063*fs
-        int n_samples;          // number of samples processed at one time
+        int n_considered;       // number of samples processed at one time
         int lr_size;            // size of "window" of values being compared to considered sample
 
         double diff_threshold;  // Theta_diff
         double min_threshold;   // Theta_min
         double avg_height;      // H_ave 
 
-        unsigned bits; 
-		unsigned N_numbers;
-		long nslots;
-		key_params params;
-		vector< vector < long > > inputs;
-		vector< vector < vector<long> > >  v_in;
-		vector< vector < mkt > > k_constant, k;
-		void t_start();
-		void t_end(string name);
-		void set_params();
-		void initialize();
-		void make_copies();
+        vector<mkt> encrypted_thresholds; // Encrypted thresholds
 
-		bool test_RCADDER();
-        bool test_RBSUBER();
-        bool test_RCMP();
-		bool test_DUALSLOPE();
-        bool test_REQ();
-        bool test_SHIFTR();
-        bool test_SHIFTL();
-        bool test_NMUX();
+        unsigned bits;          // number of bits in each sample
+		unsigned n_samples;     // total number of samples
+		long nslots;            // number of slots used in each Ciphertext (computed internally)
+		key_params params;
+
+        vector < long > scaling_factors;   // how much to scale samples by so that calculations always done on whole numbers. 
+
+        vector < long > samples_x;         // samples scaled by x (which is set in initialize()) 
+        vector < long > samples_x_10;      // samples scaled by x / 10
+        vector < long > samples_x_11;      // samples scaled by x / 11
+        vector < long > samples_x_12;      // ...
+        vector < long > samples_x_13;
+        vector < long > samples_x_14;
+        vector < long > samples_x_15;
+        vector < long > samples_x_16;
+        vector < long > samples_x_17;
+        vector < long > samples_x_18;
+        vector < long > samples_x_19;
+        vector < long > samples_x_20;
+        vector < long > samples_x_21;
+        vector < long > samples_x_22;
+        vector < long > samples_x_23;      // samples scaled by x / 23
+
+		vector < vector<long> > samples_bits_x;        // samples scaled by x converted to binary 
+		vector < vector<long> > samples_bits_x_10;     // samples scaled by x / 10 converted to binary
+		vector < vector<long> > samples_bits_x_11;     // ...
+		vector < vector<long> > samples_bits_x_12;
+		vector < vector<long> > samples_bits_x_13;
+		vector < vector<long> > samples_bits_x_14;
+		vector < vector<long> > samples_bits_x_15;
+		vector < vector<long> > samples_bits_x_16;
+		vector < vector<long> > samples_bits_x_17;
+		vector < vector<long> > samples_bits_x_18;
+		vector < vector<long> > samples_bits_x_19;
+		vector < vector<long> > samples_bits_x_20;
+		vector < vector<long> > samples_bits_x_21;
+		vector < vector<long> > samples_bits_x_22;
+		vector < vector<long> > samples_bits_x_23;     // samples scaled by x / 23 converted to binary 
+
+		vector < mkt > k_constant_x;        // samples_x encrypted - a vector of keys (see helper_functions.h)
+		vector < mkt > k_constant_x_10;     // samples_x_10 encrypted
+		vector < mkt > k_constant_x_11;     // ...
+		vector < mkt > k_constant_x_12;
+		vector < mkt > k_constant_x_13;
+		vector < mkt > k_constant_x_14;
+		vector < mkt > k_constant_x_15;
+		vector < mkt > k_constant_x_16;
+		vector < mkt > k_constant_x_17;
+		vector < mkt > k_constant_x_18;
+		vector < mkt > k_constant_x_19;
+		vector < mkt > k_constant_x_20;
+		vector < mkt > k_constant_x_21;
+		vector < mkt > k_constant_x_22;
+		vector < mkt > k_constant_x_23;     // samples_x_23 encrypted
+
+		vector< vector < long > > inputs;          //variable passed to binary circuits
+		vector< vector < vector<long> > >  v_in;   //inputs converted to bitsets to be passed to circuits
+		vector< vector < mkt > > k, k_constant;    //encrypted values passed to circuits
+
+		void t_start();                     // start the timer
+		void t_end(string name);            // end the timer
+		void set_params();                  // set parameters for BGV cryptosystem
+		void initialize();                  // prepare for calculations
+		void make_copies(vector< vector<mkt> > input, vector< vector<mkt> > destination); // copy contents of one vector<vector<mkt>> to another one
+
+        void prepare_data(vector<long> sample_subset); // prepare data for homomorphic operations. 
+
+        // Components of Dualslope algorithm broken into pieces (operating on FHE encrypted data)
+        void compute_lr_slopes(vector< vector<mkt> > encrypted_pairs, bool simd);
+        vector<mkt> compute_min_max(vector<mkt> encrypted_list);
+        vector<mkt> compute_diff_max(vector<mkt> encrypted_mins_maxs);
+        vector<mkt> compare_to_thresholds(vector<mkt> diff_maxs);
+        mkt check_peak_closeness(vector<mkt> peaks);
+        vector<mkt> update_thresholds(vector<mkt> diff_maxs); 
+
+        // Components of Dualslope algorithm broken into pieces (operating on plaintext data)
+        void compute_lr_slopes(vector< vector<long> > plain_pairs);
+        vector<long> compute_min_max(vector<long> plain_list);
+        vector<long> compute_diff_max(vector<long> plain_mins_maxs);
+        vector<bool> compare_to_thresholds(vector<long> diff_maxs);
+        long check_peak_closeness(vector<long> peaks);
+        void update_thresholds(long diff_max); 
+
+        void ds_fhe();
+        void ds_fhe(int iterations);
+        void ds_unpacked_fhe();
+        void ds_unpacked_fhe(int iterations);
+        void ds_plain();
+        void ds_plain(int iterations);
+
+        bool test_ds_fhe();
+        bool test_ds_fhe(int iterations);
+        bool test_ds_unpacked_fhe();
+        bool test_ds_unpacked_fhe(int iterations);
+        bool test_ds_plain();
+        bool test_ds_plain(int iterations);        
 };
 #endif
